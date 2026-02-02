016923da0b83ae44c2676700ce4062e838473a78
Your Name <zaidiinsiya83@gmail.com>
2026-02-02 23:13:38 +0530

    understanding logics

--- utilis/textchunker.js
+++ utilis/textchunker.js
@@ -40,17 +40,15 @@ for(let i =0 ; i<paragraphwords.length;i +=(chunksize-overlap)){   //   i = 0,  
         chunkindex:chunkindex++,
         pagenumber:0
     })
-    if(i+chunksize>= paragraphwords.length) break;  //yha pr bhi check krrha h ki agr size bda hogya toh vps iterate krne ki zaroorat nhi 
-    // eg 750+300=1050>=1000 true - break ...  
+    if(i+chunksize>= paragraphwords.length) break;  //yha pr bhi check krrha h ki agr size bda hogya toh vps iterate krne ki zaroorat nhi // eg 750+300=1050>=1000 true - break ...  
+    continue;   
+ // is para ka kaam hogya  .. isko chunk krke save krlia .. ab next paragraph pr chle jaooo  
 }
-
-continue;  // is para ka kaam hogya  .. isko chunk krke save krlia .. ab next paragraph pr chle jaooo 
-    }
-
-
-
+ 
+   
  // if adding this para exceeds chunk size , save current chunk 
-if(currentwordcount+paragraphwordcount>chunksize && currentchunk.length>0 ){  // currentchunk - 250 h aur paragraphwordcount - 80 , 250+80-310 >300 
+
+if(currentwordcount+paragraphwordcount>chunksize && currentchunk.length>0 ){  // currentchunk - 260 h aur paragraphwordcount - 80 , 250+80-310 >300 
     chunks.push({  // leave the paragraphwordcount -80 kyu  ki limit cross horhi h bs currentchunk ko save krlo ... 
             content:currentchunk.join('\n\n'),
             chunkindex: chunkindex++,
@@ -59,19 +57,31 @@ if(currentwordcount+paragraphwordcount>chunksize && currentchunk.length>0 ){  //
 
 
         // create overlap from previous chunk 
-        
-        const prevchunktext = currentchunk.join(' ')
-            const prevwords = prevchunktext.split(/\S+/)
-            const overlaptext = prevwords.slice(-Math.min(overlap , prevwords.length)).join(' ')
-            currentchunk = [overlaptext ,paragraph.trim()]
-            currentwordcount = overlaptext.split(/\S+/).length+paragraphwordcount
+
+        const prevchunktext = currentchunk.join(' ')   
+ const prevwords = prevchunktext.split(/\s+/) // create array of that 260 words prev
+  const overlaptext = prevwords.slice(-Math.min(overlap , prevwords.length)).join(' ')  // min (50 , 260) isliye kyu ki kya pta prevwords.length m 
+           // 30 hote toh last ke 50 words ni le skte the .. 
+           // taking eg .slice(50) .. yaani peece se 50 words utha lo ... 
+           
+            currentchunk = [overlaptext ,paragraph.trim()]  // ab currentchunk start hoga prev 50 words + new 80 words 
+            currentwordcount = overlaptext.split(/\S+/).length+paragraphwordcount // 50+80=130.. this prevtext was taken to ensure overlapping must take place 
         
 }
+
 else {
-    // add paragraph to current chunk 
-    currentchunk.push(paragraph.trim())
+    // add paragraph to current chunk if currentchunk can accomodate the new para 
+    currentchunk.push(paragraph.trim()) //  Agar jagah hai chunk me
+                           //toh paragraph simply add kar do
+                            //aur word count update kar do.
     currentwordcount += paragraphwordcount
 }
+
+}
+
+
+
+
 
 
 // add last chunk 
@@ -84,13 +94,19 @@ if(currentchunk.length>0){
 }
 
 // fall back if no chunks created , split by words 
-if(chunks.length ===0 && cleanedtext.length>0){
-    const allwords = cleanedtext.split(/\S+/)
-    for(let i = 0;i<allwords.length;i+=(chunksize-overlap)){
-        const chunkwords = allwords.slice(i , i+chunksize)
+if(chunks.length ===0 && cleanedtext.length>0){ // in case koi chunk bna hi nhi h Text me proper paragraph break hi nahi mila
+
+ //Pura text ek hi continuous string hai
+
+ // Paragraph-based logic fail ho gaya
+
+ // Toh:  // similar concept .. 
+    const allwords = cleanedtext.split(/\s+/)  // eg allwords.length() - 1000 h 
+    for(let i = 0;i<allwords.length;i+=(chunksize-overlap)){  // i -(0 to 300-50)
+        const chunkwords = allwords.slice(i , i+chunksize)  // slice - (0 , 299) .. 
         chunks.push(
             {
-              content:currentchunk.join('\n\n'),
+              content:chunkwords.join('\n\n'),
             chunkindex: chunkindex++,
             pagenumber:0   
             })
