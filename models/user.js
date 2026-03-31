564c76cdc759372b9e1cd531b6a911a16ddb549c
Your Name <zaidiinsiya83@gmail.com>
2026-03-31 21:47:59 +0530

    31....

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose"
 import bcrypt from "bcryptjs"
-import { defaultAllowedOrigins } from "vite";
 const userschema = new mongoose.Schema({
    username:{
     type:String ,
